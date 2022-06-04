4f958e3fbbc8f088682e781df915fe14f99beebc
Nekogami-WiN <win@nekogami.moe>
2022-06-04 02:08:46 +0200

    Added Hashing Algorithms

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Multi Hash
+import hashlib
+#Hashing Algorithms
+md5_hash = hashlib.md5()
+sha1_hash = hashlib.sha1()
+sha256_hash = hashlib.sha256()
+sha512_hash = hashlib.sha512()
+
+
+#ptf = input("Path to File: ")
+f = open(ptf)
+print(f)
