54054eec6bf57f702651f3fe15232bb583ea3926
Nekogami-WiN <win@nekogami.moe>
2022-06-04 02:25:44 +0200

    Adding some more hashing Algorithms

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ sha256_hash = hashlib.sha256()
 sha512_hash = hashlib.sha512()
 
 #ptf = input("Path to File: ")
+#read file
 f = open("test.png", "rb")
 inputfile = f.read()
+
 md5_hash.update(inputfile)
 sha1_hash.update(inputfile)
+sha256_hash.update(inputfile)
+sha512_hash.update(inputfile)
+
+#hash to hex
 md5_digest = md5_hash.hexdigest()
 sha1_digest = sha1_hash.hexdigest()
+sha256_digest = sha256_hash.hexdigest()
+sha512_digest = sha512_hash.hexdigest()
+#print hash
 print("md5: " + md5_digest)
 print("sha1: " + sha1_digest)
+print("sha256: " + sha256_digest)
+print("sha512: " + sha512_digest)
